81bc4f0a5bc6719cb24997334238feb26cfd127c
Alex <alexsveshkarev@gmail.com>
2021-12-06 16:43:38 +0200

    Visual corrected

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -3,15 +3,21 @@ package Main;
 public class Main {
     public static void main(String[] args) {
         var handler = new AnimalsHandler();
+        System.out.println("*************** Task 1 ***************");
+
         handler.task1();
+        System.out.println("*************** Task 2 ***************");
+
         try {
-            System.out.println("Min square meters in zoo "+handler.task2()+" sq. m.");
+            System.out.println("Min square meters in zoo " + handler.task2() + " sq. m.");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        System.out.println(handler.taskOptional("ground"));
-        System.out.println(handler.taskOptional("water"));
-        System.out.println(handler.taskOptional("air"));
+        System.out.println("*************** Task optional ***************");
+
+        System.out.println("Min car carrying capacity for ground animals " + handler.taskOptional("ground") + " kg.");
+        System.out.println("Min car carrying capacity for water animals " + handler.taskOptional("water") + " kg.");
+        System.out.println("Min car carrying capacity for air animals " + handler.taskOptional("air") + " kg.");
     }
 }
 
